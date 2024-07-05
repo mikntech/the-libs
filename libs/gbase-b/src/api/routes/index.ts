d8a1e73716b6d39b9f5740358c44ddb47e07941d
GitHub Action <michael+cicd@offisito.com>
2024-07-05 16:45:10 +0300

    init o step 8

--- libs/gbase-b/src/api/routes/index.ts
+++ libs/gbase-b/src/api/routes/index.ts
@@ -1,15 +1,16 @@
-import { Response, NextFunction, CookieOptions } from 'express';
+import { Response, NextFunction } from 'express';
+import { CookieOptions } from 'express-serve-static-core';
 import { AuthenticatedRequest } from 'auth-b';
 
-interface APIResponse {
+export interface APIResponse {
   code: number;
   body?: {} | string;
   cookie?: { name: string; val: string; options: CookieOptions };
 }
 
-type Write = (
+export type Write = (
   chunk: any,
-  callback?: (error: Error | null | undefined) => void
+  callback?: (error: Error | null | undefined) => void,
 ) => boolean;
 
 type RESTHandler = (req: AuthenticatedRequest) => Promise<APIResponse>;
@@ -20,33 +21,55 @@ interface WSHeaders {
   stat: string;
 }
 
-type HighOrderHandler = {
-  (handler: RESTHandler, wsHeaders?: undefined): (
-    req: AuthenticatedRequest,
-    res: Response,
-    next: NextFunction
-  ) => void;
-  (handler: WSHandler, wsHeaders: WSHeaders[]): (
-    req: AuthenticatedRequest,
-    res: Response,
-    next: NextFunction
-  ) => void;
-};
+function highOrderHandler(
+  handler: RESTHandler,
+  wsHeaders?: undefined,
+): (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction,
+) => Promise<void>;
 
-export const highOrderHandler =
-  <T extends AuthenticatedRequest>(
-    handler: (req: T) => Promise<APIResponse>,
-    wsHeaders?: WSHeaders[]
-  ) =>
-  (req: T, res: Response, next: NextFunction) => {
-    handler(req)
-      .then((response) => {
-        const { code, body, cookie } = response;
+function highOrderHandler(
+  handler: WSHandler,
+  wsHeaders: WSHeaders[],
+): (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction,
+) => Promise<void>;
+
+function highOrderHandler(
+  handler: RESTHandler | WSHandler,
+  wsHeaders?: WSHeaders[],
+) {
+  return async (
+    req: AuthenticatedRequest,
+    res: Response,
+    next: NextFunction,
+  ) => {
+    try {
+      if (wsHeaders) {
+        wsHeaders.forEach(({ path, stat }) => res.setHeader(path, stat));
+        await handler(req, res.write as any);
+        return;
+      } else {
+        const restResponse: APIResponse = await (handler as RESTHandler)(req);
+        const { code, body, cookie } = restResponse;
+        if (code >= 500) throw new Error('Internal Server Error');
+        const ret = res.status(code);
         if (cookie) {
-          res.cookie(cookie.name, cookie.val, cookie.options);
+          const { name, val, options } = cookie;
+          ret.cookie(name, val, options);
         }
-        res.status(code);
-        body ? res.json(body) : res.end();
-      })
-      .catch(next);
+        typeof body === 'string' || body === undefined
+          ? ret.send(body)
+          : ret.json(body);
+      }
+    } catch (err) {
+      next(err);
+    }
   };
+}
+
+export default highOrderHandler;
