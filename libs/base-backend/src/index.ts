ed017b6a3402031f3ca7772303c93206ce6840fd
GitHub Action <michael+cicd@offisito.com>
2024-07-08 12:19:24 +0300

    base: express perfect

--- libs/base-backend/src/index.ts
+++ libs/base-backend/src/index.ts
@@ -1,6 +1,23 @@
+
 export *  from './config';
 export * from './controllers';
 export * from './exceptions';
 export * from './schemas';
 export * from './services';
 export * from './types';
+
+
+
+import { setup } from './services';
+import { connect } from './schemas';
+import { Router } from 'express';
+import { settings } from './config';
+
+
+
+export const start = (apiRouter= Router(), watchDB =()=>{}) => {
+  console.log('Connecting to MongoDB...');
+  connect(settings.mongoURI,settings.stagingEnv,watchDB)
+    .then(() => setup(apiRouter).catch((e) => console.log(e)))
+    .catch((e) => console.log(e));
+};
