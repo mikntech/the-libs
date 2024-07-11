cf566d8b2e82833227e9b5b6fd4c370a226ce2fc
GitHub Action <michael+cicd@offisito.com>
2024-07-11 22:33:04 +0300

    stabilized 404 handler

--- libs/base-backend/src/services/express/middlewares/notFoundHandler.ts
+++ libs/base-backend/src/services/express/middlewares/notFoundHandler.ts
@@ -1,5 +1,4 @@
-import { NextFunction, Request as ExpressRequest, Response } from 'express';
-
+import { NextFunction, Request as ExpressRequest, Response } from "express";
 
 interface Layer {
   route?: {
@@ -10,38 +9,40 @@ interface Layer {
   regexp?: { source: string };
 }
 
-const extractRoutes = (layers: Layer[], basePath: string = ''): string[] => {
-  return layers.flatMap((layer) => {
-    if (layer.route) {
-      const methods = Object.keys(layer.route.methods)
-        .filter((method) => layer.route?.methods?.[method])
-        .map((method) => method.toUpperCase())
-        .join(', ');
-      return [`${methods} ${basePath}${layer.route.path}`];
-    } else if (layer.handle && layer.regexp) {
-      const routePath = formatRoutePath(layer.regexp.source);
-      return extractRoutes(layer.handle.stack, `${basePath}${routePath}`);
-    } else {
-      return [];
-    }
-  });
+const extractRoutes = (layers?: Layer[], basePath: string = ""): string[] => {
+  return (
+    layers?.flatMap((layer) => {
+      if (layer.route) {
+        const methods = Object.keys(layer.route.methods)
+          .filter((method) => layer.route?.methods?.[method])
+          .map((method) => method.toUpperCase())
+          .join(", ");
+        return [`${methods} ${basePath}${layer.route.path}`];
+      } else if (layer.handle && layer.regexp) {
+        const routePath = formatRoutePath(layer.regexp.source);
+        return extractRoutes(layer.handle.stack, `${basePath}${routePath}`);
+      } else {
+        return [];
+      }
+    }) ?? []
+  );
 };
 
 const formatRoutePath = (source: string): string => {
   return (
     source
-      .replace('^\\/', '')
-      .replace('\\/?$', '')
-      .replace('(?:\\/(?=$))?', '')
-      .replace('\\', '') ?? ''
+      .replace("^\\/", "")
+      .replace("\\/?$", "")
+      .replace("(?:\\/(?=$))?", "")
+      .replace("\\", "") ?? ""
   );
 };
 
 const filterRoutes = (routes: string[], filter: string): string[] => {
   return routes.filter((route) => {
-    const pathSegments = route.split('/');
+    const pathSegments = route.split("/");
     pathSegments.shift(); // Remove the first element, typically empty due to leading slash
-    const final = pathSegments.join('/');
+    const final = pathSegments.join("/");
     return final.startsWith(filter);
   });
 };
@@ -49,26 +50,26 @@ const filterRoutes = (routes: string[], filter: string): string[] => {
 export const autoHelper = (
   req: ExpressRequest,
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ): void => {
-  const pathSegmentsToFilter = req.originalUrl.split('/');
-  while (pathSegmentsToFilter.includes('api')) {
+  const pathSegmentsToFilter = req.originalUrl.split("/");
+  while (pathSegmentsToFilter.includes("api")) {
     pathSegmentsToFilter.shift();
   }
-  const finalToFilter = pathSegmentsToFilter.join('/');
+  const finalToFilter = pathSegmentsToFilter.join("/");
 
   if (!res.headersSent) {
     const allRoutes = extractRoutes((req.app as any)._router.stack).map(
-      (route) => route.replace('//', '/')
+      (route) => route.replace("//", "/"),
     );
     const relevantRoutes = filterRoutes(allRoutes, finalToFilter);
 
     res.status(404).json({
-      message: 'Route not found',
+      message: "Route not found",
       availableRoutes:
         relevantRoutes.length > 0
           ? relevantRoutes
-          : ['No available routes under this path'],
+          : ["No available routes under this path"],
     });
   } else {
     next();
