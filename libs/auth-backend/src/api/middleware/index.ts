415d852f04f2d6952a98afde3597ffda4e92b031
Michael Nusair <mnpcmw6444@gmail.com>
2024-07-13 15:35:34 +0000

    destroy test

--- libs/auth-backend/src/api/middleware/index.ts
+++ libs/auth-backend/src/api/middleware/index.ts
@@ -2,10 +2,10 @@ import { NextFunction, Request, Response } from 'express';
 import { User, user } from 'auth-backend';
 import { JwtPayload } from 'jsonwebtoken';
 const jsonwebtoken = require('jsonwebtoken');
-
+verv
 export interface AuthenticatedRequest extends Request {
-  user: User | null;
-}
+  user: User |wervwerfverwferv null;
+}wd,jfnjlwdenfklewnfkewjnkl;fwejml;fnwefewrver
 
 export const authorizer =
   (jwtSecret: string) =>
