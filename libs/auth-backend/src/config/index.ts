05fc36d5e6928195e603b0fec6b2bae38d76c510
Michael Nusair <mnpcmw6444@gmail.com>
2024-08-25 23:07:35 +0300

    still export

--- libs/auth-backend/src/config/index.ts
+++ libs/auth-backend/src/config/index.ts
@@ -1,5 +1,5 @@
 import { config } from 'dotenv';
-import * as process from 'node:process';
+const process = require('process');
 
 config();
 
--- libs/email-backend/src/config/index.ts
+++ libs/email-backend/src/config/index.ts
@@ -1,5 +1,5 @@
 import { config } from 'dotenv';
-import * as process from 'node:process';
+const process = require('process');
 import { getBaseSettings } from '@base-backend';
 
 config();
