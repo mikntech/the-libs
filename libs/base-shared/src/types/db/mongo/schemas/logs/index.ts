8670a7e08ffbe090b72b55f860afaa1da0f722fd
Michael Nusair <mnpcmw6444@gmail.com>
2024-08-30 22:41:59 +0300

    all

--- libs/base-shared/src/types/db/mongo/schemas/logs/index.ts
+++ libs/base-shared/src/types/db/mongo/schemas/logs/index.ts
@@ -1,6 +1,3 @@
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-
 import type { Document as MDocument, Types } from 'mongoose';
 export interface ErrorLog extends MDocument {
   _id: Types.ObjectId;
