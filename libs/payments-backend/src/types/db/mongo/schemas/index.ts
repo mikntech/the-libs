1c51a91001e1f3b9f5db8793418a7a67beb4b1b0
Michael Nusair <mnpcmw6444@gmail.com>
2024-08-30 22:03:27 +0300

    ??

--- libs/payments-backend/src/types/db/mongo/schemas/index.ts
+++ libs/payments-backend/src/types/db/mongo/schemas/index.ts
@@ -1,10 +1,6 @@
 export * from './stripe';
 
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-
-const { Types } = require('mongoose');
-import type { Document as MDocument } from 'mongoose';
+import type { Types, Document as MDocument } from 'mongoose';
 
 export interface BaseEvent extends MDocument {
   _id: Types.ObjectId;
