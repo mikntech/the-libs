126568ca45bb5d72aa5d89ba4c67f437affd4741
GitHub Action <michael+cicd@offisito.com>
2024-08-31 11:50:54 +0300

    pubkey

--- libs/base-shared/src/utils/index.ts
+++ libs/base-shared/src/utils/index.ts
@@ -29,12 +29,11 @@ export const guessValueType = (item: TODO) =>
           ? JSON.stringify(item)
           : formatTextNicely(String(item));
 
-export const handleSubscribeClick = (cb: (pushSubscription: TODO) => void) => {
+export const handleSubscribeClick = (pubkey:string,cb: (pushSubscription: TODO) => void) => {
   navigator.serviceWorker.ready.then((registration) => {
-    const base64String =
-      'BH1R9v3i49K6RwINhRAIGDWeD5Qc4P8goayR9Zse5GHr8P6TftjYECx98M-C7YBpA-DPbnM_k_QdZgQc5QnWgU8';
-    const padding = '='.repeat((4 - (base64String?.length % 4)) % 4);
-    const base64 = (base64String + padding)
+  
+    const padding = '='.repeat((4 - (pubkey?.length % 4)) % 4);
+    const base64 = (pubkey + padding)
       .replace(/-/g, '+')
       .replace(/_/g, '/');
     const rawData = window.atob(base64);
