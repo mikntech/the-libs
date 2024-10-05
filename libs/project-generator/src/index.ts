17ebfad2ee098f668d51934cae6cdbd3a5db04fe
Michael Nusair <mnpcmw6444@gmail.com>
2024-10-05 17:26:33 +0000

    mv

--- libs/project-generator/src/index.ts
+++ libs/project-generator/src/index.ts
@@ -6,6 +6,34 @@ const require = createRequire(import.meta.url);
 const inquirer = require('inquirer').default;
 import { execSync } from 'child_process';
 
+import * as fs from 'fs';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+
+// Define __filename and __dirname in ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const askForAppsAndLibs = async () => {
+  const thisDir = __dirname;
+  const libsDir = path.resolve(thisDir, '../../');
+  const libsNames = fs
+    .readdirSync(libsDir, { withFileTypes: true })
+    .filter((dirent) => dirent.isDirectory())
+    .map((dirent) => dirent.name);
+  const choices = libsNames.map((name) => ({ name, value: name }));
+  const questions = [
+    {
+      type: 'checkbox',
+      name: 'selectedItems',
+      message: 'Select @the-libs you plan to use, I will "npm i" them for you',
+      choices: choices,
+    },
+  ];
+  const answers = await inquirer.prompt(questions);
+  return answers.selectedItems;
+};
+
 const doCommand = (cmnd: string) =>
   execSync(cmnd, {
     stdio: 'inherit',
@@ -30,17 +58,17 @@ const askQuestions = async () => {
       message: 'What is the name of your new Nx project?',
     },
   ];
+  await askForAppsAndLibs();
   return inquirer.prompt(questions);
 };
 
 async function createProject() {
   const { name } = await askQuestions();
+  doCommand('npm i -g nx');
   doCommand(`npx --yes create-nx-workspace@latest ${name} \\
-  --preset=none \\
+  --preset=ts \\
   --nxCloud=skip \\
-  --ci=skip \\
   --packageManager=npm \\
-  --style=css \\
   --interactive=false \\
   --skipGit=false \\
   --npmScope=${name}`);
