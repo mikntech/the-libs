a55a21a81d1d7a2297f6d9273f097e2b8472e929
GitHub Action <michael+cicd@offisito.com>
2024-10-17 12:31:50 +0300

    i

--- apps/deploy/src/index.ts
+++ apps/deploy/src/index.ts
@@ -21,16 +21,36 @@ import { createS3Bucket } from '../../../libs/cicd-backend/src/services/aws/s3';
 import { requestCertificate } from '../../../libs/cicd-backend/src/services/aws/acm';
 import { updateSecurityGroupInboundRules } from '../../../libs/cicd-backend/src/services/aws/security';
 
+enum Staging {
+  'prod' = 'prod',
+  'preprod' = 'pre',
+  'tst' = 'tst',
+  'dev' = 'dev',
+}
+
 const DOMAIN = 'cubebox.co.il';
 const DEP_REGION = 'il-central-1';
 const projectName = 'cb';
 const apps = [
-  { name: 'server', port: 4050, domain: 'server.cubebox.co.il' },
-  { name: 'client', port: 3000, domain: 'cubebox.co.il' },
+  {
+    name: 'server',
+    port: 4050,
+    domain: 'server.cubebox.co.il',
+    exactFully: { preprod: 'preserver.cubebox.co.il' },
+  },
+  {
+    name: 'client',
+    port: 3000,
+    domain: 'cubebox.co.il',
+    exactFully: { preprod: 'pre.cubebox.co.il' },
+  },
 ];
 const appNames = apps.map(({ name }) => name);
 const nodeTag = '18.20.4';
-const stagingENVs = ['prod', 'preprod'];
+
+const stagingENVs: (keyof typeof Staging)[] = ['prod', 'preprod'];
+const stagingENVsShort: Staging[] = stagingENVs.map((name) => Staging[name]);
+
 const step1 = async () => {
   await createHostedZone(DOMAIN);
   enableRegion(DEP_REGION);
@@ -40,13 +60,21 @@ const step1 = async () => {
   const ecrUri = await getEcrUri();
 
   console.log(
-    generateYML(
-      {
-        appNames,
-      },
-      'michael@cubebox.co.il',
-      projectName,
-      DEP_REGION,
+    await Promise.all(
+      stagingENVs.map((longName: keyof typeof Staging) =>
+        generateYML(
+          {
+            appNames,
+            name: longName === 'prod' ? 'prd - release' : 'prp - main',
+            branchName: longName === 'prod' ? 'release/prod' : 'main',
+            clusterName: longName,
+            log: false,
+          },
+          'michael@cubebox.co.il',
+          projectName,
+          DEP_REGION,
+        ),
+      ),
     ),
   );
 
@@ -77,35 +105,51 @@ const step1 = async () => {
   );
   await Promise.all(
     stagingENVs.map(
-      async (env) => await createS3Bucket('cubebox-' + env, true),
+      async (longName) => await createS3Bucket('cubebox-' + longName, true),
     ),
   );
 };
+
 const step2 = async () => {
-  const certificateARNs = await Promise.all(
-    apps.map(async ({ domain }) => await requestCertificate(domain)),
-  );
-
   await Promise.all(
-    apps.map(
-      async ({ name, port }, index) =>
-        await createECSService(
-          name,
-          'prod',
-          'arn:aws:ecs:' +
-            DEP_REGION +
-            ':' +
-            (await getAccountId()) +
-            ':task-definition/mik' +
-            name +
-            ':' +
-            (name === 'server' ? 22 : 16),
-          port,
-          certificateARNs[index],
-        ),
-    ),
-  );
+    stagingENVs.map(async (longName) => {
+      const prefix = longName === 'prod' ? '' : Staging[longName];
 
+      const certificateARNs = await Promise.all(
+        apps.map(
+          async ({ domain, exactFully }) =>
+            await requestCertificate(exactFully[longName] || prefix + domain),
+        ),
+      );
+
+      await Promise.all(
+        apps.map(
+          async ({ name, port }, index) =>
+            await createECSService(
+              prefix + name,
+              longName,
+              'arn:aws:ecs:' +
+                DEP_REGION +
+                ':' +
+                (await getAccountId()) +
+                ':task-definition/mik' +
+                prefix +
+                name +
+                ':' +
+                (prefix === ''
+                  ? name === 'server'
+                    ? 22
+                    : 16
+                  : name === 'server'
+                    ? 4
+                    : 2),
+              port,
+              certificateARNs[index],
+            ),
+        ),
+      );
+    }),
+  );
   const vpcId = await getDefaultVpcId();
   const securityGroupId = await getDefaultSecurityGroupId(vpcId);
 
