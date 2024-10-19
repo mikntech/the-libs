cf522c42503084ce9b11b1acd80bc9b6f5ebce23
GitHub Action <michael+cicd@offisito.com>
2024-10-19 20:53:26 +0300

    improvements

--- libs/project-generator/src/index.ts
+++ libs/project-generator/src/index.ts
@@ -82,6 +82,69 @@ async function createProject() {
   --interactive=false \\
   --skipGit=false \\
   --npmScope=${name}`);
+  doCommand(`rm -rf ./${name}/packages`)
+  doCommand(`rm -rf ./${name}/README.md`)
+  doCommand(`rm -rf ./${name}/.gitignore`);
+  doCommand(`cat <<EOF >> ./${name}/.gitignore
+# See http://help.github.com/ignore-files/ for more about ignoring files.
+
+
+# Secrets and env
+.env
+
+
+# compiled output
+dist
+tmp
+/out-tsc
+
+
+# dependencies and platform
+node_modules
+package-lock.json
+
+
+# IDEs and editors
+/.idea
+.project
+.classpath
+.c9/
+*.launch
+.settings/
+*.sublime-workspace
+
+
+# IDE - VSCode
+.vscode/*
+!.vscode/settings.json
+!.vscode/tasks.json
+!.vscode/launch.json
+!.vscode/extensions.json
+
+
+# Misc
+/.sass-cache
+/connect.lock
+/coverage
+/libpeerconnection.log
+npm-debug.log
+yarn-error.log
+testem.log
+/typings
+
+# System Files
+.DS_Store
+Thumbs.db
+
+# Cache
+.nx
+
+# Next.js
+.next
+out
+
+EOF`);
+
 }
 
 createProject();
