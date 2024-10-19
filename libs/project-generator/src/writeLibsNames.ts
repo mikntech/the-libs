44f5e6ab5c271bacf3b4538e517867abb3e28343
GitHub Action <michael+cicd@offisito.com>
2024-10-19 14:51:29 +0300

    exclude

--- libs/project-generator/src/writeLibsNames.ts
+++ libs/project-generator/src/writeLibsNames.ts
@@ -6,16 +6,14 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Locate the src directory of packages (adjust path if necessary)
 const srcDir = path.resolve(__dirname, '../');
 
-// Read directories within src and filter out non-directories
 const libs = fs
   .readdirSync(srcDir, { withFileTypes: true })
   .filter((dirent) => dirent.isDirectory())
-  .map((dirent) => dirent.name);
+  .map((dirent) => dirent.name)
+  .filter((name) => name !== 'project-generator');
 
-// Write the library names to a JSON file for later use
 fs.writeFileSync(
   path.resolve(__dirname, 'packageNames.json'),
   JSON.stringify(libs, null, 2),
