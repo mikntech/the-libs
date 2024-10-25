6dd53b7acba6a9d0c0e02d51dbffdf7ec24effc3
GitHub Action <michael+cicd@offisito.com>
2024-10-25 22:07:19 +0300

    before

--- libs/agi-backend/src/services/openAI/index.ts
+++ libs/agi-backend/src/services/openAI/index.ts
@@ -1,6 +1,18 @@
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-const Openai = require('openai');
+import { AzureOpenAI } from 'openai';
+const apiVersion = '2024-04-01-preview';
+import {
+  DefaultAzureCredential,
+  getBearerTokenProvider,
+} from '@azure/identity';
+import { agiSettings } from '../../config';
+const credential = new DefaultAzureCredential();
+const scope = 'https://cognitiveservices.azure.com/.default';
+const azureADTokenProvider = getBearerTokenProvider(credential, scope);
+const options = {
+  azureADTokenProvider,
+  deployment: agiSettings.azureModel,
+  apiVersion,
+};
 
 export const completeWithOpenai = async (model: string, chat: any) =>
-  new Openai().completions.create({ model, prompt: chat });
+  new AzureOpenAI(options).completions.create({ model, prompt: chat });
