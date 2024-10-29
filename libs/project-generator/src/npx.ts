87e3a31c6768e33ce8aca78c2378fb1bc6d2c148
GitHub Action <michael+cicd@offisito.com>
2024-10-29 23:43:49 +0200

    b

--- libs/project-generator/src/npx.ts
+++ libs/project-generator/src/npx.ts
@@ -3,13 +3,20 @@
 //# some of the manual generation could have been done with plugins if i read this earlier: https://github.com/nrwl/nx/issues/28322#issuecomment-2402630258
 
 import { createProject } from './npx/project.js';
-import { whatToDo } from './npx/utils/questions.js';
+import { WhatToDo, whatToDo } from './npx/utils/questions.js';
+//import { createApp } from './npx/app.js';
 
 const npx = async () => {
   const answer = await whatToDo();
   switch (answer) {
-    case 'NEW_PROJECT':
+    case WhatToDo.NEW_PROJECT:
       createProject().then();
+      break;
+    case WhatToDo.NEW_APP:
+      // createApp().then();
+      break;
+    case WhatToDo.NEW_LIB:
+      // createLib().then();
       break;
   }
 };
