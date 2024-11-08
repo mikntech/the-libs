da4ed8a76037f7f69ac48731885f973e3902e086
GitHub Action <michael+cicd@offisito.com>
2024-11-08 20:40:24 +0200

    fix

--- libs/agi-backend/src/services/azureOpenAI/index.ts
+++ libs/agi-backend/src/services/azureOpenAI/index.ts
@@ -1,12 +1,5 @@
 import { agiSettings } from '../../config';
 import { createRequire } from 'module';
-import * as ChatAPI from 'openai/src/resources/chat/chat';
-import {
-  ChatCompletion,
-  ChatCompletionMessageParam,
-} from 'openai/src/resources/chat/completions';
-import { APIPromise } from 'openai/core';
-
 const require = createRequire(import.meta.url);
 const { AzureOpenAI } = require('openai');
 
@@ -19,26 +12,15 @@ const options = {
   endpoint: agiSettings.azureEndpoint,
 };
 
-// Chat completion function compatible with GPT-4o
-export const complete = async (
-  model: (string & {}) | ChatAPI.ChatModel,
-  chat: Array<ChatCompletionMessageParam>,
-) => {
-  try {
-    // Format the chat input as a message array expected by chat-completion models
-    const messages = [
-      { role: 'system', content: 'You are an AI assistant.' },
-      { role: 'user', content: chat },
-    ];
+const azureOpenAIInstance = new AzureOpenAI(options);
 
-    // Call chat-completion API instead of completion
-    const response: APIPromise<ChatCompletion> = await new AzureOpenAI(
-      options,
-    ).chat.completions.create({
-      model,
-      messages,
-    });
-    return response;
+type CreateOptionsType = Parameters<
+  typeof azureOpenAIInstance.chat.completions.create
+>[0];
+
+export const complete = async (createOptions: CreateOptionsType) => {
+  try {
+    return new AzureOpenAI(options).chat.completions.create(createOptions);
   } catch (error) {
     console.error('Error with chat completion request:', error);
     throw error;
