2f5c19112e2c798d2f95c9b38f0f2b42f6fb168c
GitHub Action <michael+cicd@offisito.com>
2024-11-10 14:45:43 +0200

    fix

--- libs/base-frontend/src/settings/index.ts
+++ libs/base-frontend/src/settings/index.ts
@@ -3,9 +3,9 @@ export interface BaseFrontendSettings {
   VITE_STAGING_ENV: string;
 }
 
-export const getFrontendSettings = <
-  FES extends BaseFrontendSettings,
->(): FES => {
+export const getFrontendSettings = <FES extends BaseFrontendSettings>(
+  keys: (keyof FES)[],
+): FES => {
   let res;
   try {
     const envConfig = document.getElementById('mik-env-config')?.textContent;
@@ -13,5 +13,9 @@ export const getFrontendSettings = <
   } catch (e) {
     res = import.meta.env;
   }
-  return res;
+  keys.forEach((key) => {
+    if (res[key] === undefined)
+      res[key] = import.meta.env[key as keyof ImportMetaEnv];
+  });
+  return res.map();
 };
