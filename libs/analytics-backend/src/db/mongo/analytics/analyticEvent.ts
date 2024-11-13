394b6e377b8f0c8fd892d7c5a0ae787f7b5d5b19
mnpcmw6444@gmail.com <michael+cicd@offisito.com>
2024-11-13 15:27:33 +0200

    freestyle avents

--- libs/analytics-backend/src/db/mongo/analytics/analyticEvent.ts
+++ libs/analytics-backend/src/db/mongo/analytics/analyticEvent.ts
@@ -2,10 +2,12 @@ import { getModel } from '@the-libs/mongo-backend';
 import { SomeEnum } from '@the-libs/base-shared';
 
 export const analyticEvent = <ENUM>(AnalyticEventEnum: SomeEnum<ENUM>) =>
-  getModel<any>('avent', {
+  getModel<any>('analyticEvent', {
     value: {
       type: String,
+      /*
       enum: Object.values(AnalyticEventEnum),
+*/
       required: true,
     },
     userNumber: {
