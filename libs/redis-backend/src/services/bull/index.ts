0b67e12ba877a73ba59f1c3e7e4c3299e24d6aab
mnpcmw6444@gmail.com <michael+cicd@offisito.com>
2024-11-19 15:25:41 +0200

    fix

--- libs/redis-backend/src/services/bull/index.ts
+++ libs/redis-backend/src/services/bull/index.ts
@@ -1,16 +1,24 @@
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
-import type { JobOptions, ProcessCallbackFunction, Queue } from 'bull';
+import {
+  DoneCallback,
+  Job,
+  JobOptions,
+  ProcessCallbackFunction,
+  Queue,
+} from 'bull';
 const BullClass = require('bull');
 import { redisSettings } from '../../config';
 
-export const createQueue = (queueName: string) =>
-  new BullClass(queueName, { redis: redisSettings.uri });
+export const createQueue = <DATA>(
+  queueName: string,
+  howToProcess: ProcessCallbackFunction<DATA>,
+  OnDone?: (job: Job<DATA>, done: DoneCallback) => void,
+) =>
+  new BullClass(queueName, { redis: redisSettings.uri }).process(
+    howToProcess,
+    OnDone,
+  );
 
 export const add = <DATA>(queue: Queue<DATA>, data: DATA, opts?: JobOptions) =>
   queue.add(data, opts);
-
-export const process = <DATA>(
-  queue: Queue<DATA>,
-  cb: ProcessCallbackFunction<DATA>,
-) => queue.process(cb);
