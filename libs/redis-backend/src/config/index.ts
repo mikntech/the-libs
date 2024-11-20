8046d5cfe9ab77cae15f4c19bcfba0ffd4dfc3e4
mnpcmw6444@gmail.com <michael+cicd@offisito.com>
2024-11-20 23:25:35 +0200

    ok

--- libs/redis-backend/src/config/index.ts
+++ libs/redis-backend/src/config/index.ts
@@ -1,11 +1,19 @@
 import { createRequire } from 'module';
+import fs from 'fs';
+import os from 'os';
+import path from 'path';
+import { spawn } from 'child_process';
+
 const require = createRequire(import.meta.url);
-
 const { config } = require('dotenv');
-const process = require('process');
-const { spawn } = require('child_process');
-
 config();
+
+function createTempPemFile(pemContent: string) {
+  const tempDir = os.tmpdir();
+  const pemPath = path.join(tempDir, 'temp_key.pem');
+  fs.writeFileSync(pemPath, pemContent, { mode: 0o600 });
+  return pemPath;
+}
 
 interface RedisURI {
   host: string;
@@ -23,39 +31,41 @@ export interface RedisSettings {
     endpoint: string;
   };
 }
+
 const port = parseInt(process.env['REDIS_PORT'] || '6379');
 const ip = process.env['REDIS_PROXY_IP'] || undefined;
 const pem = process.env['REDIS_PROXY_PEM'] || undefined;
 const endpoint = process.env['REDIS_PROXY_ENDPOINT'] || undefined;
+
 let ec2Proxy = undefined;
+
 if (ip && pem && endpoint) {
   ec2Proxy = { ip, pem, endpoint };
   const sshUser = 'ec2-user';
 
+  const pemPath = createTempPemFile(pem);
+
   const ssh = spawn(
     'ssh',
-    [
-      '-i',
-      '/dev/stdin',
-      '-L',
-      `6379:${endpoint}:${port}`,
-      `${sshUser}@${ip}`,
-      '-N',
-    ],
+    ['-i', pemPath, '-L', `6379:${endpoint}:${port}`, `${sshUser}@${ip}`, '-N'],
     {
-      stdio: ['pipe', 'inherit', 'inherit'],
+      stdio: ['inherit', 'inherit', 'inherit'],
     },
   );
 
-  ssh.stdin.write(pem);
-  ssh.stdin.end();
+  ssh.on('close', (code) => {
+    fs.unlinkSync(pemPath);
 
-  ssh.on('close', (code: number) => {
     if (code === 0) {
       console.log('SSH tunnel closed successfully.');
     } else {
       console.error(`SSH process exited with code ${code}`);
     }
+  });
+
+  ssh.on('error', (err) => {
+    console.error('Error starting SSH tunnel:', err.message);
+    fs.unlinkSync(pemPath);
   });
 }
 
@@ -66,13 +76,11 @@ export const redisSettings: RedisSettings = {
     password: process.env['REDIS_PASSWORD'] || undefined,
     user: process.env['REDIS_USER'] || undefined,
     tls:
-      process.env['REDIS_TLS'] || undefined
-        ? undefined
-        : process.env['REDIS_TLS'] === 'true'
-          ? {}
-          : {
-              servername: process.env['REDIS_TLS'],
-            },
+      process.env['REDIS_TLS'] === 'true'
+        ? {}
+        : process.env['REDIS_TLS']
+          ? { servername: process.env['REDIS_TLS'] }
+          : undefined,
   },
   ec2Proxy,
 };
