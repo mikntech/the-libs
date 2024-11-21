c8becd266af9d0c239219624c4218a62729da8ff
mnpcmw6444@gmail.com <michael+cicd@offisito.com>
2024-11-21 15:44:48 +0200

    raceCache

--- libs/redis-backend/src/services/cache/index.ts
+++ libs/redis-backend/src/services/cache/index.ts
@@ -1,21 +1,50 @@
 import type { Cluster } from 'ioredis';
 
+const withTimeout = <T>(promise: Promise<T>, ms: number): Promise<T> =>
+  Promise.race([
+    promise,
+    new Promise<T>((_, reject) =>
+      setTimeout(() => reject(new Error('Timeout')), ms),
+    ),
+  ]);
+
+export const get = async (
+  redis: Cluster,
+  key: string,
+  timeoutInMs = Infinity,
+) => {
+  try {
+    return await (timeoutInMs
+      ? withTimeout(redis.get(key), timeoutInMs)
+      : redis.get(key));
+  } catch (error) {
+    console.error(`[ERROR] Failed to fetch key: ${key}`, error);
+    return null;
+  }
+};
+
 export const set = async (
   redis: Cluster,
   key: string,
   value: string,
-  ttlInSecs: number,
-) => redis.set(key, value, 'EX', ttlInSecs);
-
-export const get = async (redis: Cluster, key: string) => redis.get(key);
+  ttlInSecs = Infinity / 1000,
+) => {
+  try {
+    await redis.set(key, value, 'EX', ttlInSecs);
+  } catch (error) {
+    console.error(`[ERROR] Failed to set key: ${key}`, error);
+    throw error;
+  }
+};
 
 export const cache = async (
   redis: Cluster,
   key: string,
   computeValue: () => Promise<string>,
-  ttlInSecs: number,
+  ttlInSecs = Infinity / 1000,
+  timeout = Infinity / 1000,
 ): Promise<string> => {
-  const cachedValue = await get(redis, key);
+  const cachedValue = await get(redis, key, timeout);
 
   if (cachedValue !== null) {
     return cachedValue;
@@ -26,4 +55,30 @@ export const cache = async (
   await set(redis, key, value, ttlInSecs);
 
   return value;
+};
+
+export const raceCache = async (
+  redis: Cluster,
+  key: string,
+  computeValue: () => Promise<string>,
+  ttlInSecs = Infinity / 1000,
+): Promise<string> => {
+  const getCachedValue = async () => get(redis, key);
+
+  const computeAndSet = async () => {
+    const value = await computeValue();
+    set(redis, key, value, ttlInSecs).then(() =>
+      console.log(`[DEBUG] Updated cache for key: ${key}`),
+    );
+    console.log(`[DEBUG] Computed cache for key: ${key}`);
+    return value;
+  };
+
+  const result = await Promise.race([getCachedValue(), computeAndSet()]);
+
+  computeAndSet().catch((err) => {
+    console.error(`[ERROR] Failed to update cache for key: ${key}`, err);
+  });
+
+  return result ?? 'Error';
 };
