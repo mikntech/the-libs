3afded8315d92ea5ca6330718c0044a837f2eb3d
mnpcmw6444@gmail.com <michael+cicd@offisito.com>
2024-11-21 15:03:05 +0200

    cache

--- libs/redis-backend/src/services/cache/index.ts
+++ libs/redis-backend/src/services/cache/index.ts
@@ -8,3 +8,22 @@ export const set = async (
 ) => redis.set(key, value, 'EX', ttlInSecs);
 
 export const get = async (redis: RedisType, key: string) => redis.get(key);
+
+export const cache = async (
+  redis: RedisType,
+  key: string,
+  computeValue: () => Promise<string>,
+  ttlInSecs: number,
+): Promise<string> => {
+  const cachedValue = await get(redis, key);
+
+  if (cachedValue !== null) {
+    return cachedValue;
+  }
+
+  const value = await computeValue();
+
+  await set(redis, key, value, ttlInSecs);
+
+  return value;
+};
