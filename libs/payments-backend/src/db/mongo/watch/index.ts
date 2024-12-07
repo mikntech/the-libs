03e9832b2aa6d403c12cdf22b4889ea9fd504569
mnpcmw6444@gmail.com <michael+cicd@offisito.com>
2024-12-07 09:49:43 +0200

    fix

--- libs/payments-backend/src/db/mongo/watch/index.ts
+++ libs/payments-backend/src/db/mongo/watch/index.ts
@@ -1,8 +1,11 @@
 import { watchStripeEvents } from './stripeEvent';
 import type { GenericListener, ChangeStreamInsertDocument } from 'mongodb';
+import { StripeEvent } from '../../../types';
 
 export const watchPayments = <
-  H extends GenericListener = (event: ChangeStreamInsertDocument) => void,
+  H extends GenericListener = (
+    event: ChangeStreamInsertDocument<StripeEvent>,
+  ) => void,
 >(
   handler: H,
 ) => watchStripeEvents(handler);
