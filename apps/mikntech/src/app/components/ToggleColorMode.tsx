2526210e46541f923f0395da9349aa792c16cc62
mnpcmw6444@gmail.com <michael+cicd@offisito.com>
2024-12-12 22:33:36 +0200

    19

--- apps/mikntech/src/app/components/ToggleColorMode.tsx
+++ apps/mikntech/src/app/components/ToggleColorMode.tsx
@@ -1,12 +1,19 @@
-import PropTypes from 'prop-types';
-
 import IconButton from '@mui/material/IconButton';
 
 import WbSunnyRoundedIcon from '@mui/icons-material/WbSunnyRounded';
 import ModeNightRoundedIcon from '@mui/icons-material/ModeNightRounded';
 import { TODO } from '@the-libs/base-shared';
 
-function ToggleColorMode({ mode, toggleColorMode, ...props }: TODO) {
+interface ToggleColorModeProps {
+  mode: "dark" | "light";
+  toggleColorMode(...args: unknown[]): unknown;
+}
+
+function ToggleColorMode({
+  mode,
+  toggleColorMode,
+  ...props
+}: ToggleColorModeProps) {
   return (
     <IconButton
       onClick={toggleColorMode}
@@ -23,10 +30,5 @@ function ToggleColorMode({ mode, toggleColorMode, ...props }: TODO) {
     </IconButton>
   );
 }
-
-ToggleColorMode.propTypes = {
-  mode: PropTypes.oneOf(['dark', 'light']).isRequired,
-  toggleColorMode: PropTypes.func.isRequired,
-};
 
 export default ToggleColorMode;
