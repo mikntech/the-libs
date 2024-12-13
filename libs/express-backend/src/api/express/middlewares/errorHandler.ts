399f45a87848b7ac941a71d6c54f652bed2a6dac
mnpcmw6444@gmail.com <michael+cicd@offisito.com>
2024-12-13 18:16:16 +0200

    good

--- libs/express-backend/src/api/express/middlewares/errorHandler.ts
+++ libs/express-backend/src/api/express/middlewares/errorHandler.ts
@@ -18,7 +18,7 @@ export const serverErrorHandler =
     SE = StagingEnvironment,
   >(
     stagingEnv: SE,
-    errorLogModel: () => Promise<ExtendedModel<DocI, unknown>> = async () =>
+    errorLogModel: () => Promise<ExtendedModel<DocI, false>> = async () =>
       (await errorLog()) as TODO,
     dontLogToMongo: boolean = false,
   ) =>
