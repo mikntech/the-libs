9b957b27deef11d8bea7cf90c0f3e7f05a2d6df8
mnpcmw6444@gmail.com <michael+cicd@offisito.com>
2024-12-14 17:32:41 +0200

    ...

--- scripts/move-index.js
+++ scripts/move-index.js
@@ -13,27 +13,11 @@ const distDir = path.join(__dirname, `../dist/libs/${libName}`);
 const srcDir = path.join(distDir, 'src');
 const indexTsFile = path.join(distDir, 'index.ts');
 
-// Create an index.ts that exports and imports types properly
-const files = fs.readdirSync(srcDir);
-const exportStatements = files
-  .filter(
-    (file) =>
-      file !== 'index.ts' && fs.lstatSync(path.join(srcDir, file)).isFile(),
-  )
-  .map((file) => {
-    // For .d.ts files, import types correctly and export them
-    if (file.endsWith('.d.ts')) {
-      return `export * from './src/${file}';`;
-    }
-    // For .ts files, export them normally
-    else if (file.endsWith('.ts')) {
-      return `export * from './src/${file}';`;
-    }
-    return '';
-  })
-  .filter(Boolean) // Filter out empty strings
-  .join('\n');
+// Create an index.ts that exports types from the .d.ts files
+const exportStatements = `export type * from './src/index.d.ts';`;
 
 // Write the new index.ts file to the root of dist/libs/base-frontend
 fs.writeFileSync(indexTsFile, exportStatements);
-console.log(`Created index.ts in the root with re-exports from src/`);
+console.log(
+  `Created index.ts in the root with 'export type *' from index.d.ts`,
+);
