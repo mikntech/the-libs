e084e2d0719d3e6979c52193d294105137609a06
mnpcmw6444@gmail.com <michael+cicd@offisito.com>
2024-12-14 16:19:21 +0200

    per

--- scripts/move-index.js
+++ scripts/move-index.js
@@ -1,23 +1,30 @@
 const fs = require('fs');
 const path = require('path');
 
+// Get the library name or directory from arguments
+const libName = process.argv[2];
+if (!libName) {
+  console.error('Please provide the library name as an argument.');
+  process.exit(1);
+}
+
 // Define paths
-const distDir = path.join(__dirname, '../dist/libs/base-frontend');
+const distDir = path.join(__dirname, `../dist/libs/${libName}`);
 const srcDir = path.join(distDir, 'src');
-const indexFile = path.join(srcDir, 'index.d.ts');
+const indexFile = path.join(distDir, 'index.ts');
 
-// Check if `index.d.ts` exists in `src`
-if (fs.existsSync(indexFile)) {
-  // Move `index.d.ts` to the root of `dist/libs/base-frontend`
-  const targetPath = path.join(distDir, 'index.d.ts');
-  fs.renameSync(indexFile, targetPath);
-  console.log(`Moved index.d.ts to: ${targetPath}`);
-} else {
-  console.log('index.d.ts not found in src folder.');
-}
+// Create a new index.ts that exports everything from the src folder
+const files = fs.readdirSync(srcDir);
+const exports = files
+  .filter(
+    (file) =>
+      file !== 'index.ts' && fs.lstatSync(path.join(srcDir, file)).isFile(),
+  )
+  .map((file) => `export * from './src/${file}';`)
+  .join('\n');
 
-// Remove the `src` folder
-if (fs.existsSync(srcDir)) {
-  fs.rmSync(srcDir, { recursive: true, force: true });
-  console.log('Removed src folder from dist directory.');
-}
+// Write the new index.ts file to the root of dist/libs/base-frontend
+fs.writeFileSync(indexFile, exports);
+console.log(`Created index.ts in the root with re-exports from src/`);
+
+// No need to remove or move files now, since we are simply creating the index.ts
