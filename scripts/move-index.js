513f432b3ede7079b5b3611046c97fc7d6588cde
mnpcmw6444@gmail.com <michael+cicd@offisito.com>
2024-12-14 17:55:03 +0200

    Revert "."
    This reverts commit 021dc4011fc7da8b683348834c91bac8a9d7224c.

--- scripts/move-index.js
+++ scripts/move-index.js
@@ -20,16 +20,11 @@ const exportStatements = files
     (file) =>
       file !== 'index.ts' && fs.lstatSync(path.join(srcDir, file)).isFile(),
   )
-  .map((file) => {
-    // Make sure we only export .ts files (excluding .d.ts files)
-    if (file.endsWith('.ts')) {
-      return `export * from './src/${file}';`;
-    }
-    return '';
-  })
-  .filter(Boolean) // Filter out empty strings
+  .map((file) => `export * from './src/${file}';`)
   .join('\n');
 
 // Write the new index.ts file to the root of dist/libs/base-frontend
 fs.writeFileSync(indexTsFile, exportStatements);
 console.log(`Created index.ts in the root with re-exports from src/`);
+
+// No need to remove or move files now, since we are simply creating the index.ts
