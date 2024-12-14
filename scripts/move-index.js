de50c6b6f285935f47ba89faed77f7b14a259657
mnpcmw6444@gmail.com <michael+cicd@offisito.com>
2024-12-14 17:54:53 +0200

    Revert "mean"
    This reverts commit c683ffc0937dcc0032fee010f093582044410c3f.

--- scripts/move-index.js
+++ scripts/move-index.js
@@ -11,35 +11,13 @@ if (!libName) {
 // Define paths
 const distDir = path.join(__dirname, `../dist/libs/${libName}`);
 const srcDir = path.join(distDir, 'src');
-const indexDtsFile = path.join(distDir, 'index.d.ts');
 const indexTsFile = path.join(distDir, 'index.ts');
 
-// Create an index.d.ts that exports types from the .d.ts files
-const typeExportStatements = `export * from './src/index.d.ts';`;
-
-// Create the index.d.ts file in the root of dist/libs/base-frontend
-fs.writeFileSync(indexDtsFile, typeExportStatements);
-console.log(
-  `Created index.d.ts in the root with type exports from src/index.d.ts`,
-);
-
-// Create an index.ts that exports actual values from the .ts files
-const tsExportStatements = fs
-  .readdirSync(srcDir)
-  .filter(
-    (file) =>
-      file !== 'index.ts' && fs.lstatSync(path.join(srcDir, file)).isFile(),
-  )
-  .map((file) => {
-    // Export only .ts files (code)
-    if (file.endsWith('.ts')) {
-      return `export * from './src/${file}';`;
-    }
-    return '';
-  })
-  .filter(Boolean) // Filter out empty strings
-  .join('\n');
+// Create an index.ts that exports types from the .d.ts files
+const exportStatements = `export type * from './src/index.d.ts';`;
 
 // Write the new index.ts file to the root of dist/libs/base-frontend
-fs.writeFileSync(indexTsFile, tsExportStatements);
-console.log(`Created index.ts in the root with exports from .ts files`);
+fs.writeFileSync(indexTsFile, exportStatements);
+console.log(
+  `Created index.ts in the root with 'export type *' from index.d.ts`,
+);
