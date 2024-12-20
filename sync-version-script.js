d0c2d03cd97e74ba6234acea74096a5b474d2cd5
mnpcmw6444@gmail.com <michael+cicd@offisito.com>
2024-12-20 12:30:00 +0100

    fix

--- sync-version-script.js
+++ sync-version-script.js
@@ -1,7 +1,9 @@
 import fs from 'fs';
 import path from 'path';
-
 import { fileURLToPath } from 'url';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
