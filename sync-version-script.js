c6dfeed6ad09c3a1dbbf495618b0f4cfcbf50e31
mnpcmw6444@gmail.com <michael+cicd@offisito.com>
2024-12-20 12:23:46 +0100

    fix

--- sync-version-script.js
+++ sync-version-script.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
 
 const rootPackageJsonPath = path.join(__dirname, './package.json');
 const rootPackageJson = require(rootPackageJsonPath);
