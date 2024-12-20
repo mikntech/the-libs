fe2df647d747d71354ac7c4278524ec25e748b9f
mnpcmw6444@gmail.com <michael+cicd@offisito.com>
2024-12-20 12:26:39 +0100

    fix

--- sync-version-script.js
+++ sync-version-script.js
@@ -1,6 +1,11 @@
 import fs from 'fs';
 import path from 'path';
 
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const rootPackageJsonPath = path.join(__dirname, './package.json');
 const rootPackageJson = require(rootPackageJsonPath);
 const rootVersion = rootPackageJson.version;
