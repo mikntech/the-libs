42702e7fab2f5548090313f1b57d9d23ac8d9c4e
mnpcmw6444@gmail.com <michael+cicd@offisito.com>
2024-12-28 22:53:41 +0200

    fin

--- apps/mikntech/next.config.mjs
+++ apps/mikntech/next.config.mjs
@@ -8,6 +8,7 @@ const nextConfig = {
     esmExternals: true,
   },
   webpack: (config) => {
+    config.experiments.outputModule = true; // Enforces ESM output
     config.output.module = true; // Enforces ESM output
     return config;
   },
