1d25f4d95957726ee9f3bc175ed8b1f3b2b4d122
mnpcmw6444@gmail.com <michael+cicd@offisito.com>
2024-12-28 23:06:12 +0200

    Revert "fin"
    This reverts commit 6119a495b87de50d3350edc1b755f02a0dfa3f55.

--- apps/mikntech/next.config.js
+++ apps/mikntech/next.config.js
@@ -4,13 +4,6 @@ config();
 const nextConfig = {
   output: 'standalone',
   typescript: { ignoreBuildErrors: true },
-  experimental: {
-    esmExternals: true,
-  },
-  webpack: (config) => {
-    config.output.module = true; // Enforces ESM output
-    return config;
-  },
 };
 
 export default nextConfig;
