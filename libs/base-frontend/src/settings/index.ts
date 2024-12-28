9ba9fa573584d14fd3e49e7c7095f62e85c0e84c
mnpcmw6444@gmail.com <michael+cicd@offisito.com>
2024-12-28 22:15:03 +0200

    .

--- libs/base-frontend/src/settings/index.ts
+++ libs/base-frontend/src/settings/index.ts
@@ -1,28 +1,9 @@
-import { removePrefix } from '@the-libs/base-shared';
-
-export const getViteSettings = (
-  keys: string[],
-  env: Record<string, string>,
-): Record<string, string> => {
-  const defaultKeys = ['VITE_NODE_ENV', 'VITE_STAGING_ENV'];
-  const combinedKeys = [...defaultKeys, ...keys];
-
-  let parsedEnv: Record<string, string> = {};
-
+export const parseProdViteEnv = () => {
   try {
-    const envConfig =
-      document.getElementById('mik-env-config')?.textContent ?? '{}';
-    parsedEnv = JSON.parse(envConfig);
-  } catch (error) {
-    console.error('Error parsing mik-env-config:', error);
-    parsedEnv = env;
+    return JSON.parse(
+      document?.getElementById?.('mik-env-config')?.textContent ?? '{}',
+    );
+  } catch {
+    return {};
   }
-
-  const result: Record<string, string> = {};
-
-  combinedKeys.forEach((key) => {
-    result[key] = removePrefix(parsedEnv[key] ?? env[key] ?? '', 'VITE_');
-  });
-
-  return result;
 };
