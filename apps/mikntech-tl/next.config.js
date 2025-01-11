926cf287a937c3b9e1e734d9df5515afecd94dc1
mnpcmw6444@gmail.com <michael+cicd@offisito.com>
2025-01-11 20:56:23 +0200

    fix

--- apps/mikntech-tl/next.config.js
+++ apps/mikntech-tl/next.config.js
@@ -1,21 +1,8 @@
-//@ts-check
+import { config } from 'dotenv';
+config();
 
-import { composePlugins, withNx } from '@nx/next';
-
-/**
- * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
- **/
 const nextConfig = {
-  nx: {
-    // Set this to true if you would like to use SVGR
-    // See: https://github.com/gregberge/svgr
-    svgr: false,
-  },
+  output: 'standalone',
 };
 
-const plugins = [
-  // Add more Next.js plugins to this list if needed.
-  withNx,
-];
-
-export default composePlugins(...plugins)(nextConfig);
+export default nextConfig;
