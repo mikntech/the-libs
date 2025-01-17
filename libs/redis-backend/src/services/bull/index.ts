e10367407feade00b63e54d09fad0f22ca35340c
mnpcmw6444@gmail.com <michael+cicd@offisito.com>
2025-01-17 21:43:15 +0200

    fix

--- libs/redis-backend/src/services/bull/index.ts
+++ libs/redis-backend/src/services/bull/index.ts
@@ -9,6 +9,7 @@ import type {
 const require = createRequire(import.meta.url);
 const BullClass = require('bull');
 import { redisSettings } from '../../config';
+import { PubSub } from '../pubsub';
 
 const queues = new Map<string, Queue>(); // Store created queues to ensure reuse
 
@@ -151,3 +152,86 @@ const handleShutdownSignal = async (signal: string) => {
 // Register signal handlers
 process.on('SIGINT', () => handleShutdownSignal('SIGINT'));
 process.on('SIGTERM', () => handleShutdownSignal('SIGTERM'));
+
+export interface BaseJob {
+  testId: string;
+}
+
+interface JobType<TD> {
+  currentStage: number;
+  taskData: TD;
+}
+
+interface StageServiceConfig<
+  StageKey extends string,
+  StageMapping extends Record<
+    string,
+    { Input: Record<string, any>; Output: Record<string, any> }
+  >,
+  TD extends BaseJob,
+> {
+  stage: StageKey;
+  indexInStages: number;
+  service: (
+    taskData: StageMapping[StageKey]['Input'] & TD,
+  ) => Promise<StageMapping[StageKey]['Output']>;
+}
+
+export const runStageAsService = <
+  StageKey extends keyof StageMapping & string,
+  StageMapping extends Record<
+    string,
+    { Input: Record<string, any>; Output: Record<string, any> }
+  >,
+  TD extends BaseJob,
+>(
+  {
+    stage,
+    indexInStages,
+    service,
+  }: StageServiceConfig<StageKey, StageMapping, TD>,
+  pubsub?: PubSub,
+  STAGE_UPDATES_CHANNEL = 'DEFAULT_STAGE_UPDATES_CHANNEL',
+) =>
+  createAndAutoProcessQueue<JobType<TD>>(stage, async (job, done) => {
+    try {
+      const { taskData, currentStage } = job.data;
+      if (currentStage !== indexInStages)
+        throw new Error(
+          'Critical orchestration error - mismatch between ' +
+            'currentStage: ' +
+            currentStage +
+            ' and MY_INDEX: ' +
+            indexInStages,
+        );
+
+      const result = await service(taskData);
+
+      await add(job.queue.name, {
+        ...taskData,
+        currentStage: currentStage + 1,
+        prevRes: result,
+      });
+
+      pubsub?.publish(
+        STAGE_UPDATES_CHANNEL,
+        JSON.stringify({ id: taskData.testId, stage: currentStage }),
+      );
+
+      done();
+    } catch (err: any) {
+      console.error(err);
+      job.failedReason = err.message;
+
+      pubsub?.publish(
+        STAGE_UPDATES_CHANNEL,
+        JSON.stringify({
+          id: job.data.taskData.testId,
+          stage: job.data.currentStage,
+          error: true,
+        }),
+      );
+
+      done(err);
+    }
+  });
