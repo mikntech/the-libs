14947f489653868d357bb257e6361d589dbf9f1c
mnpcmw6444@gmail.com <michael+cicd@offisito.com>
2025-01-17 16:48:41 +0200

    fix

--- libs/express-backend/src/api/express/routes/index.ts
+++ libs/express-backend/src/api/express/routes/index.ts
@@ -4,28 +4,37 @@ import { NotLoggedInError, TODO } from '@the-libs/base-shared';
 
 interface APIResponse {
   statusCode: number;
-  body?: {} | string;
+  body?: object | string;
   cookie?: { name: string; val: string; options: CookieOptions };
 }
 
-export const highOrderHandler = <R extends Request>(
+interface HighOrderHandlerParams<R> {
   handler:
     | ((req: R) => Promise<APIResponse>)
     | ((req: R) => APIResponse)
-    | ((req: R, write: ServerResponse['write']) => Promise<void>),
+    | ((req: R, write: ServerResponse['write']) => Promise<void>);
   wsHeaders?: {
     path: string;
     stat: string;
-  }[],
-  validateAuth = true,
+  }[];
+  validateAuth?: boolean;
+}
+
+export const highOrderHandler = <R extends Request>(
+  params:
+    | HighOrderHandlerParams<R>
+    | Pick<HighOrderHandlerParams<R>, 'handler'>,
 ) =>
   (async (req: R & { user?: TODO }, res: Response, next: NextFunction) => {
+    const { wsHeaders, validateAuth } = params as HighOrderHandlerParams<R>;
+    let { handler } = params as HighOrderHandlerParams<R>;
+    if (!handler) handler = params as typeof handler;
     try {
       if (wsHeaders) {
         wsHeaders.forEach(({ path, stat }) => res.setHeader(path, stat));
         await handler(req, res.write.bind(res));
       } else {
-        if (validateAuth && !req.user) next(new NotLoggedInError());
+        if ((validateAuth ?? true) && !req.user) next(new NotLoggedInError());
         const restResponse = await (
           handler as (req: R) => Promise<APIResponse>
         )(req);
