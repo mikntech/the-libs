b7bdeb1f58102fd5eed61ffc1bbeffc65e22c650
mnpcmw6444@gmail.com <michael+cicd@offisito.com>
2025-03-02 14:48:49 +0200

    env private

--- apps/mikntech/next.config.js
+++ apps/mikntech/next.config.js
@@ -4,6 +4,10 @@ config();
 const nextConfig = {
   output: 'standalone',
   typescript: { ignoreBuildErrors: true },
+  env: {
+    NEXT_PUBLIC_POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY,
+    NEXT_PUBLIC_POSTHOG_HOST: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+  },
 };
 
 export default nextConfig;
